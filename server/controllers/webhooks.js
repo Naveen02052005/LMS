7367f2ec03351bf410ed274cbfd0c8bea10009e7
Naveen02052005 <naveenmerugu959@gmail.com>
2026-06-13 20:33:14 +0530

    stripe fix

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -104,7 +104,7 @@ export const stripeWebhooks = async(request,response) =>{
       const paymentIntentId = paymentIntent.id;
 
       const session = await stripeInstance.checkout.sessions.list({
-        payment_intene:paymentIntentId
+        payment_intent:paymentIntentId
       })
 
       const { purchaseId } = session.data[0].metadata;
